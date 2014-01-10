8be455eacc9f7dd13ec308cafa3d76d62fcd202e
DanMeakin <dangemarkalis@gmail.com>
2014-01-10 19:53:38 +0000

    Fixed merge issues with database __init__.py file.

--- paf_tools/database/__init__.py
+++ paf_tools/database/__init__.py
@@ -5,14 +5,7 @@ database itself.
 
 """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///./paf-tools.db')
-<<<<<<< HEAD
-Base = declarative_base()
-Base.metadata.create_all(engine)
-=======
-tables.Base.metadata.create_all(engine)
->>>>>>> 68cb7fd6645460ca432f2cda8dd486d032737887
 Session = sessionmaker(bind=engine)
